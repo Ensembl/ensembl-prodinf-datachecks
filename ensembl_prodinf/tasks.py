1aa82d4aaa6a683cbe127b995afbee8c17ed5db9
Dan Staines <dstaines@ebi.ac.uk>
2017-08-07 13:07:20 +0100

    fixed some minor bugs

--- ensembl_prodinf/tasks.py
+++ ensembl_prodinf/tasks.py
@@ -4,22 +4,19 @@ import getpass
 import json
 import urllib2
 
-
-
 @app.task(bind=True)
 def email_when_complete(self, url, address):
     """ Task to check a URL and send an email once the result has a non-incomplete status 
     Used for periodically checking whether a hive job has finished.
     """
-
     # allow infinite retries 
     self.max_retries = None
     result = json.load(urllib2.urlopen(url))
     if result['status'] == 'incomplete':
         raise self.retry()
     else:
-        from_email = "%s@ebi.ac.uk" % getpass.getuser()
+        from_email_address = "%s@ebi.ac.uk" % getpass.getuser()
         smtp_server = 'localhost'
-        send_email(smtp_server, from_email, address, result['subject'], result['body'])
+        send_email(smtp_server, from_email_address, address, result['subject'], result['body'])
         return result
 
