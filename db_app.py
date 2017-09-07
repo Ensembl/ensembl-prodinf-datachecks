a40b28ec02e705646ce600a8e16d1ec9fb30a62c
Dan Staines <dstaines@ebi.ac.uk>
2017-09-07 21:03:34 +0100

    added functional status endpoint

--- db_app.py
+++ db_app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from sqlalchemy import create_engine, text
+from ensembl_prodinf.db_utils import list_databases
+from ensembl_prodinf.server_utils import get_status
 import json
 import logging
 import re
@@ -19,17 +20,20 @@ cors = CORS(app)
 json_pattern = re.compile("application/json")
 
 @app.route('/list_databases', methods=['GET'])
-def list_databases():
+def list_databases_endpoint():
     try:
         db_uri = request.args.get('db_uri')
         query = request.args.get('query')
-        logging.info("Looking up "+str(query)+" on "+str(db_uri))
-        engine = create_engine(db_uri)
-        s = text("select schema_name from information_schema.schemata where schema_name rlike :q")
-        noms = []
-        with engine.connect() as con:
-            noms = [str(r[0]) for r in con.execute(s, {"q":query}).fetchall()]
-        return jsonify(noms)
+        return jsonify(list_databases(db_uri, query))
     except ValueError:
         return "Job "+str(job_id)+" not found", 404
+
+@app.route('/status/<host>', methods=['GET'])
+def get_status_endpoint(host):
+    dir_name  = request.args.get('dir_name')
+    if(dir_name == None):
+        dir_name = '/instances'
+    print dir_name
+    print host
+    return jsonify(get_status(host=host, dir_name=dir_name))
 
