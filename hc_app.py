9f23aa2849fedefac1be19f6bc5be24c42e25379
Dan Staines <dstaines@ebi.ac.uk>
2017-09-12 13:21:58 +0100

    added delete endpoint

--- hc_app.py
+++ hc_app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from ensembl_prodinf import HiveInstance
-from ensembl_prodinf.tasks import email_when_complete
 import logging
 import re
+
+from ensembl_prodinf import HiveInstance
+from ensembl_prodinf.tasks import email_when_complete
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -29,37 +31,48 @@ json_pattern = re.compile("application/json")
 @app.route('/submit', methods=['POST'])
 def submit():
     if json_pattern.match(request.headers['Content-Type']):
-        logging.debug("Submitting HC "+str(request.json))
+        logging.debug("Submitting HC " + str(request.json))
         job = get_hive().create_job(app.analysis, request.json)
         results = {"job_id":job.job_id};
-        email =  request.json.get('email')
+        email = request.json.get('email')
         if email != None and email != '':
-            logging.debug("Submitting email request for  "+email)
-            email_results = email_when_complete.delay(request.url_root+"results_email/"+str(job.job_id),email)
+            logging.debug("Submitting email request for  " + email)
+            email_results = email_when_complete.delay(request.url_root + "results_email/" + str(job.job_id), email)
             results['email_task'] = email_results.id
         return jsonify(results);
     else:
-        return "Could not handle input of type "+request.headers['Content-Type'], 415
+        return "Could not handle input of type " + request.headers['Content-Type'], 415
 
 
 @app.route('/results/<int:job_id>', methods=['GET'])
 def results(job_id):
     try:
-        logging.info("Retrieving job with ID "+str(job_id))
+        logging.info("Retrieving job with ID " + str(job_id))
         return jsonify(get_hive().get_result_for_job_id(job_id))
     except ValueError:
-        return "Job "+str(job_id)+" not found", 404
+        return "Job " + str(job_id) + " not found", 404
+
+@app.route('/delete/<int:job_id>', methods=['GET'])
+def delete(job_id):
+    hive = get_hive()
+    job = get_hive().get_job_by_id(job_id)
+    if(job == None):
+        return "Job " + str(job_id) + " not found", 404
+    hive.delete_job(job)
+    return jsonify({"id":job_id})
 
 @app.route('/results_email/<int:job_id>', methods=['GET'])
 def results_email(job_id):
     try:
         email = request.args.get('email')
-        logging.info("Retrieving job with ID "+str(job_id)+" for "+str(email))
+        logging.info("Retrieving job with ID " + str(job_id) + " for " + str(email))
+        job = get_hive().get_job_by_id(id)
+        results['input'] = job.input_id
         results = get_hive().get_result_for_job_id(job_id)
         if results['status'] == 'complete':
             results['subject'] = 'Healthchecks for %s - %s' % (results['output']['db_name'], results['output']['status'])
             results['body'] = "Results for %s:\n" % (results['output']['db_uri'])
-            for (test,result) in results['output']['results'].iteritems():
+            for (test, result) in results['output']['results'].iteritems():
                 results['body'] += "* %s : %s\n" % (test, result['status'])
                 if result['messages'] != None:
                     for msg in result['messages']:
@@ -70,9 +83,10 @@ def results_email(job_id):
         results['output'] = None
         return jsonify(results)
     except ValueError:
-        return "Job "+str(job_id)+" not found", 404
+        return "Job " + str(job_id) + " not found", 404
 
 @app.route('/jobs', methods=['GET'])
 def jobs():
     logging.info("Retrieving jobs")
     return jsonify(get_hive().get_all_results(app.analysis))
+
