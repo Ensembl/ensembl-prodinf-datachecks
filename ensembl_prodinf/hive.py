e06b9783744e57dfdebf359d06106a5a1a64e383
Dan Staines <dstaines@ebi.ac.uk>
2017-09-13 09:24:59 +0100

    various bugfixes, mainly related to session handling

--- ensembl_prodinf/hive.py
+++ ensembl_prodinf/hive.py
@@ -60,9 +60,9 @@ class Job(Base):
     semaphore_count = Column(Integer, default=0)
     
     analysis_id = Column(Integer, ForeignKey("analysis_base.analysis_id"))
-    analysis = relationship("Analysis", uselist=False)
+    analysis = relationship("Analysis", uselist=False, lazy="joined")
 
-    result = relationship("Result", uselist=False)
+    result = relationship("Result", uselist=False, lazy="joined")
 
     log_messages = relationship("LogMessage", viewonly=True)
 
@@ -76,21 +76,30 @@ class HiveInstance:
     analysis_dict = dict()
 
     def __init__(self, url, timeout=3600):
-        engine = create_engine(url, pool_recycle=timeout)
+        engine = create_engine(url, pool_recycle=timeout, echo=False)
         Session.configure(bind=engine)
-        self.session = Session()
-
 
     def get_job_by_id(self, id):
 
         """ Retrieve a job given the unique surrogate ID """
-        return self.session.query(Job).filter(Job.job_id == id).first()
+        s = Session()
+        try:
+            job = s.query(Job).filter(Job.job_id == id).first()
+            if(job == None):
+                raise ValueError("Job %s not found" % id)
+            job.result
+            return job
+        finally:
+            s.close()
 
     def get_analysis_by_name(self, name):
 
         """ Find an analysis """
-
-        return self.session.query(Analysis).filter(Analysis.logic_name==name).first()
+        s = Session()
+        try:
+            return s.query(Analysis).filter(Analysis.logic_name==name).first()
+        finally:
+            s.close()
 
     def create_job(self, analysis_name, input_data):
 
@@ -102,17 +111,20 @@ class HiveInstance:
         analysis = self.get_analysis_by_name(analysis_name)
         if analysis == None:
             raise ValueError("Analysis %s not found" % analysis_name)
-        session = self.session
+        s = Session()
         try:
             job = Job(input_id=dict_to_perl_string(input_data), status='READY', analysis_id=analysis.analysis_id);
-            session.add(job)
-            session.commit()
+            s.add(job)
+            s.commit()
             # force load of object
             job.analysis
+            job.result
             return job
         except:
-            session.rollback()
+            s.rollback()
             raise        
+        finally:
+            s.close()
 
     def get_result_for_job_id(self, id):
 
@@ -136,7 +148,6 @@ class HiveInstance:
     def get_job_tree_status(self, job):
 
         """ Recursively check all children of a job """
-
         # check for semaphores
         if job.semaphore_count>0:
             return self.check_semaphores_for_job(job)
@@ -146,11 +157,16 @@ class HiveInstance:
             elif job.status != 'DONE':
                 return 'incomplete'
             else:
-                for child_job in self.session.query(Job).filter(Job.prev_job_id == job.job_id).all():
-                    child_status = self.get_job_tree_status(child_job)
-                    if child_status != 'complete':
-                        return child_status
-                return 'complete'
+                s = Session()
+                try:
+                    for child_job in s.query(Job).filter(Job.prev_job_id == job.job_id).all():
+                        child_status = self.get_job_tree_status(child_job)
+                        if child_status != 'complete':
+                            return child_status
+                    return 'complete'
+                finally:
+                    s.close()
+
 
     def get_semaphored_jobs(self,job,status=None):
 
@@ -159,31 +175,52 @@ class HiveInstance:
         'failed' indicates that at least one child has failed
         'incomplete' indicates that at least one child is running or ready
         """
-
-        if status == None:
-            return self.session.query(Job).filter(Job.semaphored_job_id==job.job_id).all()
-        else:
-            return self.session.query(Job).filter(Job.semaphored_job_id==job.job_id, Job.status == status).all()
+        s = Session()
+        try:
+            if status == None:
+                return s.query(Job).filter(Job.semaphored_job_id==job.job_id).all()
+            else:
+                return s.query(Job).filter(Job.semaphored_job_id==job.job_id, Job.status == status).all()
+        finally:
+            s.close()
 
     def check_semaphores_for_job(self, job):
 
         """ Find all jobs that are semaphored children of the nominated job, and check whether they have completed """
 
-        status = 'complete'
-        jobs  = dict(self.session.query(Job.status, func.count(Job.status)).filter(Job.semaphored_job_id==job.job_id).group_by(Job.status).all())
-        if 'FAILED' in jobs and jobs['FAILED']>0:
-            status = 'failed'
-        elif ('READY' in jobs and jobs['READY']>0) or ('RUN' in jobs and jobs['RUN']>0): 
-            status = 'incomplete'
-        return status
+        s = Session()
+        try:
+            status = 'complete'
+            jobs  = dict(s.query(Job.status, func.count(Job.status)).filter(Job.semaphored_job_id==job.job_id).group_by(Job.status).all())
+            if 'FAILED' in jobs and jobs['FAILED']>0:
+                status = 'failed'
+            elif ('READY' in jobs and jobs['READY']>0) or ('RUN' in jobs and jobs['RUN']>0): 
+                status = 'incomplete'
+            return status
+        finally:
+            s.close()
 
     def get_all_results(self, analysis_name):
 
         """Find all jobs from the specified analysis"""
-        jobs = self.session.query(Job).join(Analysis).filter(Analysis.logic_name == analysis_name).all()
-        return list(map(lambda job: self.get_result_for_job(job), jobs))
+        s = Session()
+        try:
+            jobs = s.query(Job).join(Analysis).filter(Analysis.logic_name == analysis_name).all()
+            return list(map(lambda job: self.get_result_for_job(job), jobs))
+        finally:
+            s.close()
         
     def delete_job(self, job):
-        self.session.delete(job)
-        
+        s = Session()
+        try:
+            print "Deleting job "+str(job.job_id)
+            if(job.result != None):
+                s.delete(job.result)
+            s.delete(job)            
+            s.commit()
+        except:
+            s.rollback()
+            raise        
+        finally:
+            s.close()
         
