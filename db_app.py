484b0dddb9b25eef8c72f70212c2c589ea8d2f48
Dan Staines <dstaines@ebi.ac.uk>
2017-12-14 22:10:30 +0000

    new endpoint just for load

--- db_app.py
+++ db_app.py
@@ -2,16 +2,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ensembl_prodinf.db_utils import list_databases
-from ensembl_prodinf.server_utils import get_status
+from ensembl_prodinf.server_utils import get_status, get_load
 from ensembl_prodinf import HiveInstance
 from ensembl_prodinf.tasks import email_when_complete
-import json
 import logging
 import re
 import os
 import signal
 import time
-
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -22,11 +20,14 @@ app.config.from_pyfile('db_config.py')
 app.analysis = app.config["HIVE_ANALYSIS"]
 
 hive = None
+
+
 def get_hive():
     global hive
     if hive == None:
         hive = HiveInstance(app.config["HIVE_URI"])
     return hive
+
 
 def is_running(pid):
     try:
@@ -35,40 +36,51 @@ def is_running(pid):
         return False
     return True
 
+
 cors = CORS(app)
 
 # use re to support different charsets
 json_pattern = re.compile("application/json")
+
 
 @app.route('/list_databases', methods=['GET'])
 def list_databases_endpoint():
     try:
         db_uri = request.args.get('db_uri')
         query = request.args.get('query')
-        logging.debug("Finding dbs matching "+query+" on "+db_uri)
+        logging.debug("Finding dbs matching " + query + " on " + db_uri)
         return jsonify(list_databases(db_uri, query))
     except ValueError:
-        return "Job "+str(job_id)+" not found", 404
+        return "Could not list databases", 500
+
 
 @app.route('/status/<host>', methods=['GET'])
 def get_status_endpoint(host):
-    dir_name  = request.args.get('dir_name')
+    dir_name = request.args.get('dir_name')
     if(dir_name == None):
         dir_name = '/instances'
-    logging.debug("Finding status of "+host+" (dir "+dir_name+")")
+    logging.debug("Finding status of " + host + " (dir " + dir_name + ")")
     return jsonify(get_status(host=host, dir_name=dir_name))
+
+
+@app.route('/load/<host>', methods=['GET'])
+def get_load_endpoint(host):
+    logging.debug("Finding load of " + host)
+    return jsonify(get_load(host=host))
+
 
 @app.route('/list_servers/<user>', methods=['GET'])
 def list_servers_endpoint(user):
-    query  = request.args.get('query')
+    query = request.args.get('query')
     servers = app.config["SERVER_URIS"]
     if user in servers:
-        logging.debug("Finding servers matching "+query+" for "+user)
+        logging.debug("Finding servers matching " + query + " for " + user)
         user_urls = servers[user] or []
         urls = filter(lambda x:query in x, user_urls)
         return jsonify(urls)
     else:
-        return "User "+user+" not found", 404
+        return "User " + user + " not found", 404
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -94,14 +106,16 @@ def results(job_id):
     except ValueError:
         return "Job " + str(job_id) + " not found", 404
 
+
 @app.route('/failure/<int:job_id>', methods=['GET'])
 def failure(job_id):
     try:
         logging.info("Retrieving failure for job with ID " + str(job_id))
-        failure=get_hive().get_job_failure_msg_by_id(job_id)
+        failure = get_hive().get_job_failure_msg_by_id(job_id)
         return jsonify({"msg":failure.msg})
     except ValueError:
         return "Job " + str(job_id) + " not found", 404
+
 
 @app.route('/kill_job/<int:job_id>', methods=['GET'])
 def kill_job(job_id):
@@ -118,9 +132,10 @@ def kill_job(job_id):
     time.sleep(5)
     # Check if the process that we killed is alive.
     if (is_running(int(process_id.process_id))):
-        return "Wasn't able to kill the process: "+str(process_id.process_id), 404
+        return "Wasn't able to kill the process: " + str(process_id.process_id), 404
     else:
         return jsonify({"process_id":process_id.process_id})
+
 
 @app.route('/delete/<int:job_id>', methods=['GET'])
 def delete(job_id):
@@ -130,6 +145,7 @@ def delete(job_id):
         return "Job " + str(job_id) + " not found", 404
     hive.delete_job(job)
     return jsonify({"id":job_id})
+
 
 @app.route('/results_email/<int:job_id>', methods=['GET'])
 def results_email(job_id):
@@ -144,12 +160,13 @@ def results_email(job_id):
         results['body'] = "Copy from %s to %s is successful\n" % (results['output']['source_db_uri'], results['output']['target_db_uri'])
         results['body'] += "Copy took %s" % (results['output']['runtime'])
     elif results['status'] == 'failed':
-        failure=get_hive().get_job_failure_msg_by_id(job_id)
+        failure = get_hive().get_job_failure_msg_by_id(job_id)
         results['subject'] = 'Copy database from %s to %s failed' % (results['input']['source_db_uri'], results['input']['target_db_uri'])
         results['body'] = 'Copy failed with following message:\n'
         results['body'] += '%s' % (failure.msg)
     results['output'] = None
     return jsonify(results)
+
 
 @app.route('/jobs', methods=['GET'])
 def jobs():
