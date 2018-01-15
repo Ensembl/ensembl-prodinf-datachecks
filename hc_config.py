78bcda14d3e96026c58304dc3f44ba7695cbe3f4
Dan Staines <dstaines@ebi.ac.uk>
2018-01-15 16:44:31 +0000

    fixed incorrect get args

--- hc_config.py
+++ hc_config.py
@@ -1,2 +1,10 @@
-DEBUG = False
-HIVE_ANALYSIS = 'RunStandaloneHealthcheckFactory'
+import os
+DEBUG = os.environ.get("DEBUG", 'false')
+if DEBUG.lower() in ("f", "false"):
+    DEBUG = False
+elif DEBUG.lower() in ("t", "true"):
+    DEBUG = True
+
+HIVE_ANALYSIS = os.environ.get("HIVE_ANALYSIS", 'RunStandaloneHealthcheckFactory')
+HIVE_URI = os.environ.get("HIVE_URI", None)
+
