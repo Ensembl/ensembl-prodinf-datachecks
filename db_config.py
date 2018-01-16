f7190ea9c11742107ded26e480351337eb02d83b
Dan Staines <dstaines@ebi.ac.uk>
2018-01-16 20:25:21 +0000

    fix incorrect variable name

--- db_config.py
+++ db_config.py
@@ -1,2 +1,12 @@
-DEBUG = False
-HIVE_ANALYSIS = 'copy_database'
+import os
+
+DEBUG = os.environ.get("DEBUG", 'false')
+if DEBUG.lower() in ("f", "false"):
+    DEBUG = False
+elif DEBUG.lower() in ("t", "true"):
+    DEBUG = True
+
+HIVE_ANALYSIS = os.environ.get("HIVE_ANALYSIS", 'copy_database')
+HIVE_URI = os.environ.get("HIVE_URI", None)
+
+SERVER_URIS_FILE = os.environ.get("SERVER_URIS_FILE", 'server_uris.json')
