e2805880608abf720d4498156a402e222598de3c
Dan Staines <dstaines@ebi.ac.uk>
2018-04-18 09:45:24 +0100

    make config include optional

--- ensembl_prodinf/email_celery_app.py
+++ ensembl_prodinf/email_celery_app.py
@@ -1,12 +1,15 @@
 from celery import Celery
-import email_celery_app_config
+import logging
 
 app = Celery('ensembl_prodinf',
              include=['ensembl_prodinf.email_tasks'])
 
 # Load the externalised config module from PYTHONPATH
-app.config_from_object('email_celery_app_config')
-
+try:
+    import email_celery_app_config
+    app.config_from_object('email_celery_app_config')
+except:
+    logging.warning('Celery email requires email_celery_app_config module')
 
 if __name__ == '__main__':
     app.start()
