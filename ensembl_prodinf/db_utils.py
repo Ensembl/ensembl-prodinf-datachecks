976d1b1c509e88d3ddba18174cbd1274d8cb0111
Dan Staines <danstaines@users.noreply.github.com>
2018-04-25 11:43:25 +0100

    Added documentation

--- ensembl_prodinf/db_utils.py
+++ ensembl_prodinf/db_utils.py
@@ -4,7 +4,12 @@ from server_utils import get_file_sizes
 from sqlalchemy.engine.url import make_url
 
 def list_databases(db_uri, query):
-
+    """ 
+    List databases on a specified MySQL server
+    Arguments:
+      db_uri : URI of MySQL server e.g. mysql://user@host:3306/
+      query : optional regular expression to filter databases e.g. .*_core_.*
+    """
     if(db_uri.startswith('mysql')==False):
         raise ValueError('list_databases can only work with MySQL databases')
 
@@ -17,6 +22,13 @@ def list_databases(db_uri, query):
         return [str(r[0]) for r in con.execute(s, {"q":query}).fetchall()]
     
 def get_database_sizes(db_uri, query, dir_name):
+    """ 
+    List sizes of databases on a specified MySQL server
+    Arguments:
+      db_uri : URI of MySQL server e.g. mysql://user@host:3306/ (file system must be accessible)
+      query : optional regular expression to filter databases e.g. .*_core_.*
+      dir_name : location of MySQL data files on server
+    """
     db_list = list_databases(db_uri, query)
     url = make_url(db_uri)
     dir_name = dir_name + '/' + str(url.port) + '/data'
