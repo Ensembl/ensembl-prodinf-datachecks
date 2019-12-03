c417b1177ee274650a80d37fd75aaed5c53322e8
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2019-12-03 16:01:01 +0000

    Refactor datacheck app config

--- ensembl/datacheck/config.py
+++ ensembl/datacheck/config.py
@@ -1,12 +1,23 @@
 import os
 from ensembl.config import EnsemblConfig
+from ensembl_prodinf.config import load_yaml
+
+
+config_file_path = os.environ.get('DATACHECK_CONFIG_PATH')
+if config_file_path:
+    file_config = load_yaml(config_file_path)
+else:
+    file_config = {}
+
 
 class DatacheckConfig(EnsemblConfig):
-  DATACHECK_INDEX = os.path.join(EnsemblConfig.BASE_DIR, 'ensembl-datacheck/lib/Bio/EnsEMBL/DataCheck/index.json')
-  DATACHECK_COMMON_DIR = os.environ.get("DATACHECK_COMMON_DIR", None)
-  DATACHECK_CONFIG_DIR = os.path.join(DATACHECK_COMMON_DIR, 'config')
-  DATACHECK_REGISTRY_DIR = os.path.join(DATACHECK_COMMON_DIR, 'registry')
-  DATACHECK_REGISTRY_META = os.path.join(DATACHECK_REGISTRY_DIR, 'registry_meta.pm')
-  HIVE_ANALYSIS = os.environ.get("HIVE_ANALYSIS", 'DataCheckSubmission')
-  HIVE_VERT_URI = os.environ.get("HIVE_VERT_URI", None)
-  HIVE_NONVERT_URI = os.environ.get("HIVE_NONVERT_URI", None)
+    DATACHECK_INDEX = os.path.join(EnsemblConfig.BASE_DIR, 'ensembl-datacheck/lib/Bio/EnsEMBL/DataCheck/index.json')
+    DATACHECK_COMMON_DIR = os.environ.get("DATACHECK_COMMON_DIR",
+                                          file_config.get('datacheck_common_dir'))
+    DATACHECK_CONFIG_DIR = os.path.join(DATACHECK_COMMON_DIR, 'config')
+    DATACHECK_REGISTRY_DIR = os.path.join(DATACHECK_COMMON_DIR, 'registry')
+    DATACHECK_REGISTRY_META = os.path.join(DATACHECK_REGISTRY_DIR, 'registry_meta.pm')
+    HIVE_ANALYSIS = os.environ.get("HIVE_ANALYSIS",
+                                   file_config.get('hive_analysis', 'DataCheckSubmission'))
+    HIVE_VERT_URI = os.environ.get("HIVE_VERT_URI", file_config.get('hive_vert_uri'))
+    HIVE_NONVERT_URI = os.environ.get("HIVE_NONVERT_URI", file_config.get('hive_nonvert_uri'))
