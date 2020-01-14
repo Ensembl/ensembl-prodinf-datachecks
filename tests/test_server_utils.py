6f1963c3a3f2e56e9eeb7a04da0ecc5fcb3f2aa4
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2020-01-14 16:26:33 +0000

    Better URI validation

--- tests/test_server_utils.py
+++ tests/test_server_utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ensembl_prodinf import get_status
+import ensembl_prodinf.server_utils as su
 
 import unittest
 import logging
@@ -8,7 +9,7 @@ import logging
 logging.basicConfig()
 
 class ServerTest(unittest.TestCase):
-    
+
     def test_status(self):
         status = get_status()
         logging.info(status)
@@ -41,4 +42,44 @@ class ServerTest(unittest.TestCase):
         self.assertTrue(status["disk_used_pct"]<=100)
 
 
-        
+class AssertTest(unittest.TestCase):
+
+    def test_raises_assert_http_uri(self):
+        self.assertRaises(ValueError, su.assert_http_uri, '')
+        self.assertRaises(ValueError, su.assert_http_uri, 'invalid_uri')
+        self.assertRaises(ValueError, su.assert_http_uri, 'http://uri-with-no-slash')
+        self.assertRaises(ValueError, su.assert_http_uri, 'mysql://wrong-schema')
+
+    def test_passes_assert_http_uri(self):
+        su.assert_http_uri('http://server-name/')
+        su.assert_http_uri('https://server-name:port/')
+
+    def test_raises_assert_mysql_uri(self):
+        self.assertRaises(ValueError, su.assert_mysql_uri, '')
+        self.assertRaises(ValueError, su.assert_mysql_uri, 'invalid_uri')
+        self.assertRaises(ValueError, su.assert_mysql_uri, 'http://wrong_schema')
+        self.assertRaises(ValueError, su.assert_mysql_uri, 'mysql://invalid')
+        self.assertRaises(ValueError, su.assert_mysql_uri, 'mysql://user@server-no-slash')
+        self.assertRaises(ValueError, su.assert_mysql_uri, 'mysql://user:pass@server-no-port/')
+
+    def test_passes_assert_mysql_uri(self):
+        su.assert_mysql_uri('mysql://user@server:3006/')
+        su.assert_mysql_uri('mysql://user:pass@server:3306/')
+
+    def test_raises_assert_mysql_db_uri(self):
+        self.assertRaises(ValueError, su.assert_mysql_db_uri, '')
+        self.assertRaises(ValueError, su.assert_mysql_db_uri, 'invalid_uri')
+        self.assertRaises(ValueError, su.assert_mysql_db_uri, 'http://wrong_schema')
+        self.assertRaises(ValueError, su.assert_mysql_db_uri, 'mysql://invalid')
+        self.assertRaises(ValueError, su.assert_mysql_db_uri, 'mysql://user:pass@server-no-slash')
+        self.assertRaises(ValueError, su.assert_mysql_db_uri, 'mysql://user:pass@server-no-db:3306/')
+
+    def test_pass_assert_mysql_db_uri(self):
+        su.assert_mysql_db_uri('mysql://user:pass@server:3306/db_name')
+
+    def test_raises_assert_email(self):
+        self.assertRaises(ValueError, su.assert_email, '')
+        self.assertRaises(ValueError, su.assert_email, 'invalid_email.com')
+
+    def test_passes_assert_email(self):
+        su.assert_email('valid.email@domain.ac.uk')
