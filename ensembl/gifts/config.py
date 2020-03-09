e9dc2baeaaef3ad906219c60d43a405da9da9cf8
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2020-03-09 09:20:33 +0000

    Update config to comply with supervisored services

--- ensembl/gifts/config.py
+++ ensembl/gifts/config.py
@@ -1,15 +1,28 @@
 import os
 from ensembl.config import EnsemblConfig
+from ensembl_prodinf.config import load_config_yaml
 
+
+config_file_path = os.environ.get('GIFTS_CONFIG_PATH')
+file_config = load_config_yaml(config_file_path)
 
 class GIFTsConfig(EnsemblConfig):
-    HIVE_UPDATE_ENSEMBL_ANALYSIS = os.environ.get("HIVE_UPDATE_ENSEMBL_ANALYSIS", 'submit')
-    HIVE_PROCESS_MAPPING_ANALYSIS = os.environ.get("HIVE_PROCESS_MAPPING_ANALYSIS", 'submit')
-    HIVE_PUBLISH_MAPPING_ANALYSIS = os.environ.get("HIVE_PUBLISH_MAPPING_ANALYSIS", 'copy_database')
-    HIVE_UPDATE_ENSEMBL_URI = os.environ.get("HIVE_UPDATE_ENSEMBL_URI", None)
-    HIVE_PROCESS_MAPPING_URI = os.environ.get("HIVE_PROCESS_MAPPING_URI", None)
-    HIVE_PUBLISH_MAPPING_URI = os.environ.get("HIVE_PUBLISH_MAPPING_URI", None)
-    GIFTS_API_URIS_FILE = os.environ.get("GIFTS_APIS_URIS_FILE", 'gifts_api_uris.json')
+    BASE_DIR = os.environ.get('BASE_DIR',
+                              file_config.get('base_dir'))
+    HIVE_UPDATE_ENSEMBL_ANALYSIS = os.environ.get("HIVE_UPDATE_ENSEMBL_ANALYSIS", 
+                                                  file_config.get('hive_update_ensembl_analysis', 'submit'))
+    HIVE_PROCESS_MAPPING_ANALYSIS = os.environ.get("HIVE_PROCESS_MAPPING_ANALYSIS", 
+                                                   file_config.get('hive_process_mapping_analysis', 'submit'))
+    HIVE_PUBLISH_MAPPING_ANALYSIS = os.environ.get("HIVE_PUBLISH_MAPPING_ANALYSIS", 
+                                                   file_config.get('hive_publish_mapping_analysis', 'copy_database'))
+    HIVE_UPDATE_ENSEMBL_URI = os.environ.get("HIVE_UPDATE_ENSEMBL_URI", 
+                                             file_config.get('hive_update_ensembl_uri', None))
+    HIVE_PROCESS_MAPPING_URI = os.environ.get("HIVE_PROCESS_MAPPING_URI",
+                                              file_config.get('hive_process_mapping_uri', None))
+    HIVE_PUBLISH_MAPPING_URI = os.environ.get("HIVE_PUBLISH_MAPPING_URI", 
+                                              file_config.get('hive_publish_mapping_analysis', None))
+    GIFTS_API_URIS_FILE = os.environ.get("GIFTS_APIS_URIS_FILE", 
+                                         file_config.get('gifts_api_uris_file', 'gifts_api_uris.json')
 
     SWAGGER = EnsemblConfig.SWAGGER
     SWAGGER['title'] = 'Ensembl Production: GIFTs Pipeline API'
