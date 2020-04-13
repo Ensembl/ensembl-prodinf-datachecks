60ab4b30f8c1d31d1028278c6abad11a8990711a
James Allen <jallen@ebi.ac.uk>
2020-04-13 12:26:26 +0100

    Remove 'release' and 'config_profile' parameters, these are no longer required. Rename 'database' to 'dbname', for consistency with hive pipeline convention.

--- ensembl/datacheck/client.py
+++ ensembl/datacheck/client.py
@@ -11,26 +11,24 @@ from ensembl.server_utils import assert_mysql_uri, assert_mysql_db_uri
 class DatacheckClient(RestClient):
   """Client for checking databases using the datacheck service"""
 
-  def submit_job(self, server_url, database, species, division, db_type, release,
+  def submit_job(self, server_url, dbname, species, division, db_type,
                  datacheck_names, datacheck_groups, datacheck_types,
-                 config_profile, email, tag):
+                 email, tag):
     """
     Run datachecks on a given server, for one or more species.
     Parameter requirements are complicated, because only the server_url is absolutely required,
     for lots of other parameters you need one from a set, but it doesn't matter which one...
     Arguments:
       server_url - location of server, in URI format
-      database - name of a database to check
+      dbname - name of a database to check
       species - name of a species to check
       division - name of a division to check
       db_type - type of database to check, defaults to 'core'
-      release - release number of database to check, defaults to current development version
 
       datacheck_names - names of datacheck(s) to run, multiple values must be comma-separated
       datacheck_groups - datacheck group(s) to run, multiple values must be comma-separated
       datacheck_types - optional filter on type, 'critical' or 'advisory'
 
-      config_profile - a division name, used to load appropriate parameters
       email - optional address for an email on job completion
       tag - optional text for grouping datacheck submissions
     """
@@ -38,23 +36,16 @@ class DatacheckClient(RestClient):
 
     payload = {
       'server_url': server_url,
-      'database': database,
-      'species': None,
-      'division': None,
+      'dbname': dbname,
+      'species': species,
+      'division': division,
       'db_type': db_type,
-      'release': release,
       'datacheck_names': [],
       'datacheck_groups': [],
       'datacheck_types': [],
-      'config_profile': config_profile,
       'email': email,
       'tag': tag
     }
-
-    if species is not None:
-      payload['species'] = species.split(',')
-    elif division is not None:
-      payload['division'] = division.split(',')
 
     if datacheck_names is not None:
       payload['datacheck_names'] = datacheck_names.split(',')
@@ -130,8 +121,9 @@ class DatacheckClient(RestClient):
   def print_inputs(self, i):
     """Utility to render a job input dict to logging"""
     logging.info("Registry file: " + i['registry_file'])
-    if 'database' in i:
-      logging.info("Database name: " + i['database'])
+    if 'dbname' in i:
+      for dbname in i['dbname']:
+        logging.info("Database name: " + dbname)
     if 'species' in i:
       for species in i['species']:
         logging.info("Species name: " + species)
@@ -140,8 +132,6 @@ class DatacheckClient(RestClient):
         logging.info("Division name: " + division)
     if 'db_type' in i:
       logging.info("Database type: " + i['db_type'])
-    if 'release' in i:
-      logging.info("Ensembl release: " + i['release'])
     if 'datacheck_names' in i:
       for name in i['datacheck_names']:
         logging.info("Datacheck: " + name)
@@ -151,8 +141,6 @@ class DatacheckClient(RestClient):
     if 'datacheck_types' in i:
       for datacheck_type in i['datacheck_types']:
         logging.info("Datacheck type: " + datacheck_type)
-    if 'config_profile' in i:
-      logging.info("Config profile: " + i['config_profile'])
     if 'email' in i:
       logging.info("Email: " + i['email'])
     if 'tag' in i:
@@ -170,15 +158,13 @@ if __name__ == '__main__':
   parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
   parser.add_argument('-o', '--output_file', help='File to write output as JSON', type=argparse.FileType('w'))
   parser.add_argument('-s', '--server_url', help='URL of database server', required=True)
-  parser.add_argument('-db', '--database', help='Database name')
+  parser.add_argument('-db', '--dbname', help='Database name')
   parser.add_argument('-sp', '--species', help='Species production name')
   parser.add_argument('-div', '--division', help='Division')
   parser.add_argument('-dbt', '--db_type', help='Database type')
-  parser.add_argument('-r', '--release', help='Ensembl release version')
   parser.add_argument('-n', '--datacheck_names', help='Datacheck names, multiple names comma-separated')
   parser.add_argument('-g', '--datacheck_groups', help='Datacheck groups, multiple names comma-separated')
   parser.add_argument('-dct', '--datacheck_types', help='Datacheck type (advisory or critical)')
-  parser.add_argument('-c', '--config_profile', help='Division for configuration')
   parser.add_argument('-e', '--email', help='Email address for pipeline reports')
   parser.add_argument('-t', '--tag', help='Tag to collate results and facilitate filtering')
   parser.add_argument('-f', '--failure_only', help='Show failures only', action='store_true')
@@ -190,15 +176,12 @@ if __name__ == '__main__':
   else:
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-  if args.uri.endswith('/') == False:
-    args.uri = args.uri + '/'
-
   client = DatacheckClient(args.uri)
 
   if args.action == 'submit':
-    job_id = client.submit_job(args.server_url, args.database, args.species, args.division, args.db_type, args.release,
+    job_id = client.submit_job(args.server_url, args.dbname, args.species, args.division, args.db_type,
                                args.datacheck_names, args.datacheck_groups, args.datacheck_types,
-                               args.config_profile, args.email, args.tag)
+                               args.email, args.tag)
     logging.info('Job submitted with ID ' + str(job_id))
 
   elif args.action == 'retrieve':
