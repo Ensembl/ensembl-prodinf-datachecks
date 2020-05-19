e9ef64e336b42ae8daf85affd1820d95c2f59030
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2020-05-19 12:27:55 +0100

    Remove unused tests

--- tests/test_reporting.py
+++ tests/test_reporting.py
@@ -1,58 +1,8 @@
-import logging
 import unittest
-# import threading
-# import json
-# from ensembl_prodinf.reporting import ContextFilter, JsonFormatter
 from ensembl_prodinf.reporting import make_report, ReportFormatter
 
-logging.basicConfig()
 
 class UtilsTest(unittest.TestCase):
-    #  def test_filter(self):
-    #      context = {
-    #        'host' : "myhost",
-    #        'process' : "myprocess",
-    #        'resource' : "myres"
-    #      }
-
-    #      f = ContextFilter(context)
-    #      msg = "Hello world"
-    #      record = logging.LogRecord("logger", 20, None, "1", msg, None, None, None)
-    #      print(record.levelname)
-    #      result = f.filter(record)
-    #      self.assertTrue(result, "Checking filter return")
-    #      self.assertEqual(record.host, context['host'])
-    #      self.assertEqual(record.process, context['process'])
-    #      self.assertEqual(record.resource, context['resource'])
-    #      self.assertEqual(record.msg, msg)
-    #      context['process'] = "mynewprocess"
-    #      msg = "Goodbye world"
-    #      record = logging.LogRecord("logger", 20, None, "1", msg, None, None, None)
-    #      result = f.filter(record)
-    #      self.assertTrue(result, "Checking filter return")
-    #      self.assertEqual(record.host, context['host'])
-    #      self.assertEqual(record.process, context['process'])
-    #      self.assertEqual(record.resource, context['resource'])
-    #      self.assertEqual(record.msg, msg)
-
-    #  def test_formatter(self):
-    #      msg = "Hello world"
-    #      record = logging.LogRecord("logger", 20, None, "1", msg, None, None, None)
-    #      record.host = "myhost"
-    #      record.process = "myprocess"
-    #      record.resource = "myres"
-    #      record.report_time = "2017-01-01T06:00:01"
-    #      record.params = {"key":"val"}
-    #      print(record.levelname)
-    #      f = JsonFormatter()
-    #      json_str = f.format(record)
-    #      report = json.loads(json_str)
-    #      self.assertEqual(record.host, report['host'])
-    #      self.assertEqual(record.process, report['process'])
-    #      self.assertEqual(record.resource, report['resource'])
-    #      self.assertEqual(record.report_time, report['report_time'])
-    #      self.assertDictEqual(record.params, report['params'])
-
     def test_make_report(self):
         expected = {
             'params': {'test_param': 'test'},
