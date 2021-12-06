cd0338776fa7e788194390b4dd42654bd0e6529a
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-12-06 10:59:28 +0000

    Added unittests (moved tests package where it should be)

--- src/ensembl/production/datacheck/config.py
+++ src/ensembl/production/datacheck/config.py
@@ -11,17 +11,17 @@
 #    limitations under the License.
 
 import os
+import pathlib
 import warnings
 
 import requests.exceptions
-import sys
 from ensembl.production.core.config import load_config_yaml
-import pathlib
+
 from ensembl.utils.rloader import RemoteFileLoader
 
 pathlib.Path(__file__).parent.absolute()
 
-config_file_path = os.environ.get('DATACHECK_CONFIG_PATH')
+config_file_path = os.environ.get('DATACHECK_CONFIG_PATH', os.path.dirname(__file__) + '/datachecks_config.dev.yaml')
 file_config = load_config_yaml(config_file_path)
 
 
@@ -30,20 +30,21 @@ class DCConfigLoader:
     uri = base_uri + 'release/{}/lib/Bio/EnsEMBL/DataCheck/index.json'
 
     @classmethod
-    def load_config(cls, version=''):
+    def load_config(cls, version=None):
         loader = RemoteFileLoader('json')
-        uri = cls.uri.format(version)
-        try:
-            return loader.r_open(uri)
-        except requests.exceptions.RequestException as e:
-            warnings.warn(f"Load versioned index.json error: {e}")
-            uri = cls.base_uri + 'main/lib/Bio/EnsEMBL/DataCheck/index.json'
-            # should always be available uri.
-            return loader.r_open(uri)
+        if version:
+            uri = cls.uri.format(version)
+            try:
+                return loader.r_open(uri)
+            except requests.exceptions.RequestException as e:
+                warnings.warn(f"Load versioned index.json error: {e}")
+        uri = cls.base_uri + 'main/lib/Bio/EnsEMBL/DataCheck/index.json'
+        # should always be available uri.
+        return loader.r_open(uri)
 
 
 class EnsemblConfig:
-    ENS_VERSION = os.environ.get("ENS_VERSION", '500')
+    ENS_VERSION = os.environ.get("ENS_VERSION")
     SCRIPT_NAME = os.environ.get('SCRIPT_NAME', '')
     BASE_DIR = os.environ.get('BASE_DIR',
                               file_config.get('base_dir'))
@@ -73,10 +74,12 @@ class DatacheckConfig(EnsemblConfig):
                                    file_config.get('hive_analysis', 'DataCheckSubmission'))
     HIVE_URI = os.environ.get("HIVE_URI", file_config.get('hive_uri'))
     SERVER_NAMES_FILE = os.environ.get("SERVER_NAMES", file_config.get('server_names_file',
-                                                                       os.path.join(os.path.dirname(__file__),
-                                                                                    'server_names.dev.json')))
+                                                                       os.path.join(
+                                                                           os.path.dirname(__file__),
+                                                                           'server_names.dev.json')))
     SWAGGER_FILE = os.environ.get("SWAGGER_FILE",
-                                  file_config.get('swagger_file', f"{pathlib.Path().absolute()}/swagger.yml"))
+                                  file_config.get('swagger_file',
+                                                  f"{pathlib.Path().absolute()}/swagger.yml"))
     COPY_URI_DROPDOWN = os.environ.get("COPY_URI_DROPDOWN",
                                        file_config.get('copy_uri_dropdown',
                                                        "http://production-services.ensembl.org:80/"))
