c326cde7aba0fe67d26898fd62bce0ef5ccaa6af
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-12-06 13:47:35 +0000

    Removed unused imports from tests.

--- src/tests/test_config_loader.py
+++ src/tests/test_config_loader.py
@@ -9,10 +9,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import os
 import unittest
-from os import getenv, putenv
-from os.path import dirname
 
 from ensembl.production.datacheck.config import DCConfigLoader
 
