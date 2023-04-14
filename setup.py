3f03c6e6c0bd694ce5707e520a99afb748907b88
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2023-04-14 17:33:31 +0100

    Fall back to compara main configuration if no release set. Resolved DeprecationWarning about WTform.Regexp "DeprecationWarning: invalid escape sequence \w"

--- setup.py
+++ setup.py
@@ -9,10 +9,11 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+import os
 from pathlib import Path
 
-from setuptools import setup, find_namespace_packages, find_packages
- 
+from setuptools import setup, find_namespace_packages
+
 with open(Path(__file__).parent / 'README.md') as f:
     readme = f.read()
 with open(Path(__file__).parent / 'VERSION') as f:
@@ -25,14 +26,13 @@ def import_requirements():
         return [line.rstrip() for line in file.readlines()]
 
 
-
 setup(
     name='datacheck',
-    version=version,
+    version=os.getenv('CI_COMMIT_TAG', version),
     namespace_packages=['ensembl'],
     packages=find_namespace_packages(where='src', include=['ensembl.*']),
-    package_dir={'': 'src'}, 
-    include_package_data=True,   
+    package_dir={'': 'src'},
+    include_package_data=True,
     url='https://github.com/Ensembl/ensembl-prodinf-datacheck',
     license='APACHE 2.0',
     author='vinay kaikala',
